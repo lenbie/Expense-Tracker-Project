34021b4d825fe781ec8efa5cb3f772f9552542c1
Lene Bierstedt <schmetti3000@web.de>
2023-04-02 16:33:35 +0300

    adding expense entity

---
+++ src/entities/expense.py
@@ -0,0 +1,20 @@
+from datetime import date
+from entities.category import Category
+
+class Expense:
+    """"
+    
+    Class representing an Expense entered into the Expense tracker
+
+    Attributes:
+        name (string): The name given to the expense by the user
+        amount (float): The monetary amount of the expense
+        date: The date of the expense, default being the current system date
+        category: The category of the expense, default being undefined
+
+    """
+    def __init__(self, name = str, amount = float, date= date.today(), category = "undefined"):
+        self.name = name
+        self.amount = amount
+        self.date = date
+        self.category = category
