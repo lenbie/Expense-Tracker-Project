cddaf613cb3d99f2e9dee20b710e0c7f80318d3e
lenbie <98839926+lenbie@users.noreply.github.com>
2023-04-02 12:39:14 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -10,5 +10,5 @@ except FileNotFoundError:
 EXPENSES_FILE=os.getenv("EXPENSE_FILE") or "expenses.csv"
 EXPENSES_FILE_PATH=os.path.join(dirname, "..", "data", EXPENSES_FILE)
 
-DATABASE_FILENAME = os.getenv("DATABASE_FILENAME") or "database.sqlite"
-DATABASE_FILE_PATH = os.path.join(dirname, "..", "data", DATABASE_FILENAME)
+DATABASE_FILE = os.getenv("DATABASE_FILE") or "database.sqlite"
+DATABASE_FILE_PATH = os.path.join(dirname, "..", "data", DATABASE_FILE)
