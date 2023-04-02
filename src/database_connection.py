e3acc28b1273b0352f2ac619f19bada7cfed80e1
lenbie <98839926+lenbie@users.noreply.github.com>
2023-04-02 13:36:07 +0300

    Update database_connection.py

--- src/database_connection.py
+++ src/database_connection.py
@@ -1,7 +1,7 @@
 import sqlite3
-from config import EXPENSES_FILE_PATH
+from config import DATABASE_FILE_PATH
 
-connection = sqlite3.connect(EXPENSES_FILE_PATH)
+connection = sqlite3.connect(DATABASE_FILE_PATH)
 connection.row_factory=sqlite3.Row
 
 def connect_to_database():
