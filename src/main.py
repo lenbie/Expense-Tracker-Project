500e9a33742f87e964842a236a602faf4172c72b
Lene Bierstedt <schmetti3000@web.de>
2023-04-18 20:46:13 +0300

    pylint fixes

--- src/main.py
+++ src/main.py
@@ -5,8 +5,8 @@ def main():
     window = Tk()
     window.title("Expense Tracker")
 
-    ui = UI(window)
-    ui.start()
+    user_interface = UI(window)
+    user_interface.start()
     window.mainloop()
 
 if __name__ == "__main__":
